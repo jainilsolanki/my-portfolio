47e5a9102c3a723d0c9cc4463c432661235b2890
jainilsolanki <jainilsolanki0704@gmail.com>
2024-07-22 15:58:27 +0530

    updated resume name

--- data/utils.ts
+++ data/utils.ts
@@ -1,6 +1,6 @@
 export function downloadPdf() {
   var link = document.createElement("a");
-  link.href = "/resume.pdf";
+  link.href = "/JAINIL SOLANKI.pdf";
   link.download = "JAINIL SOLANKI.pdf";
   link.click();
   // window.open("/resume.pdf", "_blank");
